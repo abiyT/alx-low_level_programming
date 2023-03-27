b2671b1f0dacab3c3046932e8c281c9a06cd62fc
abiyT <abiytem@gmail.com>
2023-03-27 05:55:19 -0700

    test

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,15 @@
-nclude <stdio.h>
+#include <stdio.h>
 /**
- * main - A prog that print var comp
- * Return 0
- */
+ * main - datatype prinf
+ * Return: alayws 0 (success) 
+*/
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float f;
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of a long: %d byte(s)\n", sizeof(int));
+	printf("Size of a long  int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long log int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 
-printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-printf("Size of a long: %lu byte(s)\n", (unsigned long)sizeof(b));
-printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
